0217a4240015eca9070343c9fc83aaba20cb9d53
sudett <sudett4978@gmail.com>
2022-12-23 19:02:00 +0330

    Build store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,21 @@
+import { Row, Col } from "react-bootstrap";
+
+import StoreItem from "../components/StoreItem";
+import items from "../data/items.json";
+
 const Store = () => {
-  return <div>Store</div>;
+  return (
+    <>
+      <h1>Store</h1>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {items.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
 
 export default Store;
